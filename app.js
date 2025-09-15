5ba0aebe9ec54361d042cbafffb490f804c925fb
apekshathakre2507 <apeksha_thakre@hyperminds.tech>
2025-09-15 23:14:13 +0530

    change url

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ connectToDb()
 app.use(cors())
 app.use(express.json());
 
-app.use("/api", authRoute);
-app.use("/api/profile", profileRoute);
-app.use("/api/connection", connectionRoute);
-app.use("/api/user", userRoute);
+app.use("/", authRoute);
+app.use("/profile", profileRoute);
+app.use("/connection", connectionRoute);
+app.use("/user", userRoute);
 
 
 app.get("/", (req, res) => {
     res.send("Backend is running 🚀");
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
 });
